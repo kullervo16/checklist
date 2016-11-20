99c08c6a28bb060959a9ef2f1f8d486835b56d5c
jef <kullervo16@gmail.com>
2016-11-20 16:18:50 +0100

    Add backend integration

--- src/main/webapp/clientjs/app.js
+++ src/main/webapp/clientjs/app.js
@@ -825,28 +825,31 @@
         
         $http.get('rest/tags')
             .success(function (data,status,headers,config) {
-                
-                $scope.rawData = data.entries;    
-                $scope.beginLetters = [];
-                $scope.groupedData = {};
-                // now calculate the begin letters and split
-                for(var i=0;i<$scope.rawData.length;i++) {
-                    var beginLetter = $scope.rawData[i].text.toUpperCase().substring(0,1);
-                    if(!contains($scope.beginLetters, beginLetter)) {
-                        $scope.beginLetters.push(beginLetter);
-                        $scope.groupedData[beginLetter] = [];
-                    }
-                    $scope.groupedData[beginLetter].push($scope.rawData[i]);
-                }
-                $scope.beginLetters.sort();
-                for(var i=0;i<$scope.beginLetters.length;i++) {
-                    $scope.groupedData[scope.beginLetters[i]].sort();
-                }
-                console.log($scope.beginLetters);
-                console.log($scope.groupedData);
+                load(data);                
             }).error(function (data,status,headers,config) {
                 console.log('Error getting rest/tags');
             });
+            
+        function load(data) {
+            $scope.rawData = data.entries;    
+            $scope.beginLetters = [];
+            $scope.groupedData = {};
+            // now calculate the begin letters and split
+            for(var i=0;i<$scope.rawData.length;i++) {
+                var beginLetter = $scope.rawData[i].text.toUpperCase().substring(0,1);
+                if(!contains($scope.beginLetters, beginLetter)) {
+                    $scope.beginLetters.push(beginLetter);
+                    $scope.groupedData[beginLetter] = [];
+                }
+                $scope.groupedData[beginLetter].push($scope.rawData[i]);
+            }
+            $scope.beginLetters.sort();
+            for(var i=0;i<$scope.beginLetters.length;i++) {
+                $scope.groupedData[scope.beginLetters[i]].sort();
+            }
+            console.log($scope.beginLetters);
+            console.log($scope.groupedData);
+        }
             
         var contains = function(haystack,needle) {
             var indexOf = Array.prototype.indexOf;
@@ -885,7 +888,28 @@
         function checkEditState(modal) {
             if(actionOK()) {
                 if(confirm("Are you sure you want to "+$scope.selectedAction+" "+$scope.selectedTag+"? This action cannot be undone...")) {
-                    alert($scope.selectedAction+" "+$scope.selectedTag+" "+$scope.newName+" "+$scope.selectedMergeCandidate);
+                    var command = 'rest/tags/'+$scope.selectedTag;
+                    if($scope.selectedAction === 'merge') {
+                        command += '?mergeInto='+$scope.selectedMergeCandidate;
+                    } else if($scope.selectedAction === 'rename') {
+                        command += '?renameTo='+$scope.newName;
+                    }
+                    $http.delete(command).success(function (data,status,headers,config) {
+                        $http.get('rest/tags')
+                            .success(function (data,status,headers,config) {
+                                load(data); 
+                            }).error(function (data,status,headers,config) {
+                                console.log('Error getting rest/tags');
+                            });                  
+                    }).error(function (data,status,headers,config) {
+                       if( status === 409) {
+                           alert(data.description);
+                           console.log(data.description);
+                       }else {
+                           console.log('Unexpected error in delete tag !');
+                       }
+                   });
+                   
                 }
                 $('#editModal').modal('hide');
                 $('body').removeClass('modal-open');
