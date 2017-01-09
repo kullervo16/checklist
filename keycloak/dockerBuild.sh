9786406ba67e366f0ec48cb4b0eab42c52ef12d7
kernixski <kernixski@kernix.be>
2017-01-09 17:35:30 +0100

    Update keycloak/keycloak for kernixski

--- keycloak/dockerBuild.sh
+++ keycloak/dockerBuild.sh
@@ -1,1 +1,7 @@
-sudo docker build -t kullervo16/keycloak4cl .
+DOCKER_CMD="sudo docker"
+
+if [ "${USER}" = "kernixski" ]; then
+    DOCKER_CMD="docker"
+fi
+
+${DOCKER_CMD} build -t kullervo16/keycloak4cl .
