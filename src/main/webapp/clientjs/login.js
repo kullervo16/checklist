fd0a51113ec8a8766962a1f55fa1c9887bb77936
jef <kullervo16@gmail.com>
2017-01-16 18:41:08 +0100

    use calculated config

--- src/main/webapp/clientjs/login.js
+++ src/main/webapp/clientjs/login.js
@@ -1,27 +1,32 @@
 
 
 
-var keycloakConfig = {
-    "realm": "checklist",
-    "url": "http://keycloak:8085/auth",
-    "ssl-required": "none",
-    "clientId": "checklist",
-    "public-client": true
-};
+//var keycloakConfig = {
+//    "realm": "checklist",
+//    "url": "http://keycloak:8085/auth",
+//    "ssl-required": "none",
+//    "clientId": "checklist",
+//    "public-client": true
+//};
 
-window._keycloak = Keycloak(keycloakConfig);
+$.getJSON('KeycloakConfig', function(keycloakConfig) {
+    console.log(keycloakConfig);
+    window._keycloak = Keycloak(keycloakConfig);
 
-console.log("Start login process");
+    console.log("Start login process");
 
-window._keycloak.init({
-    onLoad: 'login-required'
-}).success(function () {
-    //User has logged in
-    console.log("User has successfully logged in");
-    angular.bootstrap(document, ['checklist']); // manually bootstrap Angular
-}).error(function () {
-    //User has logged in
-    console.log("User did not log in like it should");
-    alert("Cannot load the application when you are not logged in");
-    window.location.reload();
+    window._keycloak.init({
+        onLoad: 'login-required'
+    }).success(function () {
+        //User has logged in
+        console.log("User has successfully logged in");
+        angular.bootstrap(document, ['checklist']); // manually bootstrap Angular
+    }).error(function () {
+        //User has logged in
+        console.log("User did not log in like it should");
+        alert("Cannot load the application when you are not logged in");
+        window.location.reload();
+    });
 });
+
+
