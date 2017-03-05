8a0c45ca91010a201531ca035f418555bb0b6c23
jef <kullervo16@gmail.com>
2017-03-05 15:40:27 +0100

    Selenium test for milestone selection

--- src/test/java/kullervo16/checklist/selenium/BaseSeleniumTest.java
+++ src/test/java/kullervo16/checklist/selenium/BaseSeleniumTest.java
@@ -37,7 +37,7 @@ public abstract class BaseSeleniumTest {
 
     @After
     public void tearDown() throws Exception {
-        driver.close();
+        //driver.close();
     }
 
     /**
--- src/test/java/kullervo16/checklist/selenium/SelectTest.java
+++ src/test/java/kullervo16/checklist/selenium/SelectTest.java
@@ -25,7 +25,7 @@ public class SelectTest extends BaseSeleniumTest {
     }
     
     @Test
-    public void testSelectionWithFilter() throws Exception {
+    public void testTagSelectionWithFilter() throws Exception {
         getPage("select.html");
         assertEquals("Log in to Checklist",driver.getTitle());
         
@@ -54,6 +54,44 @@ public class SelectTest extends BaseSeleniumTest {
        assertTrue(driver.getCurrentUrl().contains("252ec25f-8996-4abb-924c-df667c611ec7"));
     }
     
-    
+    @Test
+    public void testMilestoneSelection() throws Exception {
+        getPage("select.html");
+        assertEquals("Log in to Checklist",driver.getTitle());
+        
+        login("alice", "secret");
+        
+        assertEquals("Tags & milestones",driver.getTitle());
+        
+        Thread.sleep(1000); //tagcloud takes some time to display
+        assertTrue(getDynamicElement("milestones", true).getText().contains("verified"));    
+        assertTrue(getDynamicElement("milestones", true).getText().contains("deployed")); 
+
+        // now click on verified
+        getDynamicElement("milestones_word_3").click();
+        Thread.sleep(1000); //tagcloud takes some time to display
+        assertEquals("deployed", getDynamicElement("milestones").getText()); // assert only 1 tag left
+       
+        // now clear selection
+        getDynamicElement("milestone_clear").click();
+        Thread.sleep(1000); //tagcloud takes some time to display
+        assertTrue(getDynamicElement("milestones", true).getText().contains("verified"));    
+        assertTrue(getDynamicElement("milestones", true).getText().contains("deployed")); 
+        
+        // now click on deployed
+        getDynamicElement("milestones_word_0").click();
+        Thread.sleep(1000); //tagcloud takes some time to display
+        // now click on verified
+        getDynamicElement("milestones_word_0").click();
+        Thread.sleep(1000); //tagcloud takes some time to display
+      
+
+        // now click goto -> end up in the overview screen
+        getDynamicElement("milestone_goto").click();
+        Thread.sleep(500);
+        
+        assertEquals("Checklist overview", driver.getTitle()); 
+        assertTrue(driver.getCurrentUrl().contains("milestones=deployed,verified"));
+    }
 
 }
